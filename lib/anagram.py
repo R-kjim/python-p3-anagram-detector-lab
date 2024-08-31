a064628764852d29e9202e5bff0d38e98ddf152d
R-kjim <rkayjim@gmail.com>
2024-08-31 20:11:35 +0300

    anagram detector python

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+class Anagram:
+    pass
+    def __init__(self,word):
+        self.name=word
+
+    def match(self,input_list):
+        pass
+        match_list=[]
+        output_list=[]
+        word_list=[n for n in self.name]
+        for item in input_list:
+            match_list=[d for d in item]
+            if sorted(match_list)==sorted(word_list):
+                output_list.append(item)
+        return output_list
+
